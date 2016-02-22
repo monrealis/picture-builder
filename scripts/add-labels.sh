60f005a32e542e8cbc5f647ff326094834138dbe
Vytenis Bivainis <monrealis@gmail.com>
2016-02-23 00:35:17 +0200

    Add script to add labels.

---
+++ scripts/add-labels.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+mkdir -p gallery
+./download-all.sh
+
+cat gallery.txt  | gawk -F\; 'BEGIN{ORS=""}{print "gallery/" $1 "\0" $2 }' | xargs -0 -n1 echo
+
+#convert barrel.png -gravity South -strokewidth 2 -font Liberation-Sans  -stroke black -fill white  -pointsize 80 -annotate 0  'das Fass\ncask' temp1.jpg
+#xdg-open temp1.jpg
