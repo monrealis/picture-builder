83654399dead7a9e820cb0b189235cddff24764f
Vytenis Bivainis <monrealis@gmail.com>
2016-02-22 21:39:44 +0200

    Add initial download script.

---
+++ scripts/download.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#der Schleim - http://media2.giga.de/2013/07/perfekter-Schleim-rcm992x0.jpg
+#das Kraut - http://www.goebbertsgardencenter.com/uploads/2012/07/fresh-herb-basket.jpg
+#die Wurzel - http://www.duden.de/_media_/full/W/Wurzel-201020526889.jpg
+url="http://vignette1.wikia.nocookie.net/mario/images/6/6a/DKCR_Artwork_Fass.png/revision/latest?cb=20101008162755&path-prefix=de"
+file=barrel.png
+if [ -f $file ];
+then
+    echo "File $file exists."
+else
+    echo "File $file does not exist."
+    curl --silent $url > $file
+fi
+
