b6dba6e95e431c16d1477077773d799cec1972ee
Vytenis Bivainis <monrealis@gmail.com>
2016-02-23 01:22:07 +0200

    Add more images and fix script.

--- scripts/build-gallery.sh
+++ scripts/build-gallery.sh
@@ -5,7 +5,7 @@ mkdir -p gallery-output
 
 ./download-all.sh
 
-cat gallery.txt  | gawk -F\; 'BEGIN{ORS=""}{print "gallery/"$1 "\0" "gallery-output/"$1".jpg" "\0" $2 }' | xargs -0 -n3 ./add-text.sh
+cat gallery.txt  | gawk -F\; 'BEGIN{ORS="\0"}{print "gallery/"$1 "\0" "gallery-output/"$1".jpg" "\0" $2 }' | xargs -0 -n3 ./add-text.sh
 
 #convert barrel.png -gravity South -strokewidth 2 -font Liberation-Sans  -stroke black -fill white  -pointsize 80 -annotate 0  'das Fass\ncask' temp1.jpg
 #xdg-open temp1.jpg
