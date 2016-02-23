0d7751b0b5047f8072a180dcddd13d73f6cadebf
Vytenis Bivainis <monrealis@gmail.com>
2016-02-24 01:27:24 +0200

    Resize to fit rectangle bounds.

--- scripts/add-text.sh
+++ scripts/add-text.sh
@@ -9,4 +9,4 @@ inputFile=$1
 outputFile=$2
 text=$3
 
-convert $inputFile -resize 800 -gravity South -strokewidth 2 -font Liberation-Sans  -stroke black -fill white -pointsize 80 -annotate 0  "$text" $outputFile
+convert $inputFile -resize 800x640 -gravity South -strokewidth 1 -font Liberation-Sans  -stroke black -fill white -pointsize 60 -annotate 0  "$text" $outputFile
