de9735d1e9ffc4edd262f858458b576678740aae
Vytenis Bivainis <monrealis@gmail.com>
2016-03-12 00:49:34 +0200

    Add loop experiment.

--- letters/letters.sh
+++ letters/letters.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-letters="AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ0123456789"
-echo $letters | grep -o . | cat -n
-
 mkdir -p output
+
+letters="AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ0123456789"
+index=1
+for letter in $(echo $letters | grep -o .) ; do
+	echo $letter $index
+	index=$[$index + 1]
+done
 convert -strokewidth 2 -font Liberation-Sans -stroke black -fill white -pointsize 80 label:One output/final.jpg
