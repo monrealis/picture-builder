5cfc3f5e4402c76ffb1785769352c48519ed1c59
Vytenis Bivainis <monrealis@gmail.com>
2016-03-12 00:51:35 +0200

    Make images of letters.

--- letters/letters.sh
+++ letters/letters.sh
@@ -5,7 +5,6 @@ mkdir -p output
 letters="AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ0123456789"
 index=1
 for letter in $(echo $letters | grep -o .) ; do
-	echo $letter $index
+	convert -strokewidth 2 -font Liberation-Sans -stroke black -fill white -pointsize 80 "label:$letter" output/$index-$letter.jpg
 	index=$[$index + 1]
 done
-convert -strokewidth 2 -font Liberation-Sans -stroke black -fill white -pointsize 80 label:One output/final.jpg
