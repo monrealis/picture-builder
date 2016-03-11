5b0d4fc690b3f2ff6745b53614f7d68f16026f3e
Vytenis Bivainis <monrealis@gmail.com>
2016-03-12 01:02:14 +0200

    Draw letters.

--- letters/letters.sh
+++ letters/letters.sh
@@ -5,6 +5,6 @@ mkdir -p output
 letters="AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ0123456789"
 index=1
 for letter in $(echo $letters | grep -o .) ; do
-	convert -strokewidth 2 -font Liberation-Sans -stroke black -fill white -pointsize 80 "label:$letter" output/$index-$letter.jpg
+	convert -resize 800x640 -strokewidth 5 -font Liberation-Sans -stroke black -fill white -pointsize 480 "label: $letter " output/$index-$letter.jpg
 	index=$[$index + 1]
 done
