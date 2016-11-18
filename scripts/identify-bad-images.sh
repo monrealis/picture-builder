626ea02a02bb2f10f3d43dc899ac68094d46fa8c
Vytenis Bivainis <monrealis@gmail.com>
2016-11-19 00:42:49 +0200

    Use the path that can be piped.

--- scripts/identify-bad-images.sh
+++ scripts/identify-bad-images.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-cd gallery
-ls | xargs -i bash -c "identify {} > /dev/null 2> /dev/null || echo {}"
-# ls | xargs -i bash -c "identify {} > /dev/null 2> /dev/null || echo {}" | xargs rm
+find gallery -type f | xargs -i bash -c "identify {} > /dev/null 2> /dev/null || echo {}"
